bcc6c5e34f27fff77f00fb302533f6e4e09c63f4
daniel-lujan <daniel.lujan@udea.edu.co>
2023-09-25 21:55:11 -0500

    feat: :sparkles: Add link navigation to sidebar options

--- components/sidebar.jsx
+++ components/sidebar.jsx
@@ -13,10 +13,13 @@ import {
 } from "@mui/material";
 import LoginIcon from "@mui/icons-material/Login";
 import HowToRegIcon from "@mui/icons-material/HowToReg";
+import { useRouter } from "next/navigation";
 
 const drawerWidth = 240;
 
 export default function Sidebar() {
+  const router = useRouter();
+
   return (
     <Drawer
       variant="permanent"
@@ -30,7 +33,17 @@ export default function Sidebar() {
       <Box sx={{ overflow: "auto" }}>
         <List>
           {["Iniciar Sesión", "Registrarse"].map((text, index) => (
-            <ListItem key={text} disablePadding>
+            <ListItem
+              key={text}
+              disablePadding
+              onClick={() => {
+                if (index === 0) {
+                  router.push("/login");
+                } else {
+                  router.push("/register");
+                }
+              }}
+            >
               <ListItemButton>
                 <ListItemIcon>
                   {index === 0 ? <LoginIcon /> : <HowToRegIcon />}
