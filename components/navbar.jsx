5f900dcf989e1e3aaed853d065345c91b3db08e8
daniel-lujan <daniel.lujan@udea.edu.co>
2023-09-26 10:25:51 -0500

    perf: :fire: Remove unused import

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -1,7 +1,6 @@
 "use client";
 
 import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import IconButton from "@mui/material/IconButton";
